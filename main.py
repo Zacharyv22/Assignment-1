5ea69fd43d84a30bc6d654504c313cfa8b0bca60
Zacharyv22 <vandykez@oregonstate.edu>
2024-09-26 13:38:26 -0700

    Initial Commit Commit2

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+# Author: Zachary Taylor Van Dyke
+# GitHub username: Zacharyv22
+# Date: 9/26/2024
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
