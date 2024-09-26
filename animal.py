056a8e8b5d7c8008c05d14729f5ff1d26b666f3b
Zacharyv22 <vandykez@oregonstate.edu>
2024-09-26 14:10:01 -0700

    Initial Commit

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Zachary Taylor Van Dyke
+# GitHub username: Zacharyv22
+# Date: 9/26/2024
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
